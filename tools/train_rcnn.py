5dcd73b4d3b94e4f50b5662d941e53ebb20e0ef6
Jian Guo <guojian710@gmail.com>
2016-07-21 18:05:00 +0800

    change to module api

--- tools/train_rcnn.py
+++ tools/train_rcnn.py
@@ -4,15 +4,18 @@ import os
 
 import mxnet as mx
 
+from rcnn.callback import Speedometer
 from rcnn.config import config
 from rcnn.loader import ROIIter
-from rcnn.solver import Solver
+from rcnn.metric import AccuracyMetric, LogLossMetric, SmoothL1LossMetric
+from rcnn.module import MutableModule
 from rcnn.symbol import get_vgg_rcnn
 from utils.load_data import load_rpn_roidb
 from utils.load_model import load_checkpoint, load_param
 from utils.save_model import save_checkpoint
 
 config.TRAIN.BG_THRESH_LO = 0.0
+config.TRAIN.ASPECT_GROUPING = False
 
 
 def train_net(image_set, year, root_path, devkit_path, pretrained, epoch,
@@ -34,7 +37,7 @@ def train_net(image_set, year, root_path, devkit_path, pretrained, epoch,
                          ctx=ctx, work_load_list=work_load_list)
 
     # infer max shape
-    max_data_shape = [('data', (1, 3, 1000, 1000))]
+    max_data_shape = [('data', (config.TRAIN.BATCH_IMAGES, 3, 1000, 1000))]
 
     # load pretrained
     args, auxs = load_param(pretrained, epoch, convert=True)
@@ -44,16 +47,46 @@ def train_net(image_set, year, root_path, devkit_path, pretrained, epoch,
         input_shapes = {k: v for k, v in train_data.provide_data + train_data.provide_label}
         arg_shape, _, _ = sym.infer_shape(**input_shapes)
         arg_shape_dict = dict(zip(sym.list_arguments(), arg_shape))
-        args['cls_score_weight'] = mx.random.normal(mean=0, stdvar=0.01, shape=arg_shape_dict['cls_score_weight'])
+        args['cls_score_weight'] = mx.random.normal(0, 0.01, shape=arg_shape_dict['cls_score_weight'])
         args['cls_score_bias'] = mx.nd.zeros(shape=arg_shape_dict['cls_score_bias'])
-        args['bbox_pred_weight'] = mx.random.normal(mean=0, stdvar=0.001, shape=arg_shape_dict['bbox_pred_weight'])
+        args['bbox_pred_weight'] = mx.random.normal(0, 0.001, shape=arg_shape_dict['bbox_pred_weight'])
         args['bbox_pred_bias'] = mx.nd.zeros(shape=arg_shape_dict['bbox_pred_bias'])
 
+    # prepare training
+    fixed_params_names = []
+    for name in args.keys():
+        if config.TRAIN.FINETUNE and name.startswith('conv'):
+            fixed_params_names.append(name)
+        elif name.startswith('conv1') or name.startswith('conv2'):
+            fixed_params_names.append(name)
+    data_names = [k[0] for k in train_data.provide_data]
+    label_names = [k[0] for k in train_data.provide_label]
+    batch_end_callback = Speedometer(train_data.batch_size, frequent=frequent)
+    epoch_end_callback = mx.callback.do_checkpoint(prefix)
+    if config.TRAIN.HAS_RPN is True:
+        eval_metric = AccuracyMetric(use_ignore=True, ignore=-1)
+        cls_metric = LogLossMetric(use_ignore=True, ignore=-1)
+    else:
+        eval_metric = AccuracyMetric()
+        cls_metric = LogLossMetric()
+    bbox_metric = SmoothL1LossMetric()
+    eval_metrics = mx.metric.CompositeEvalMetric()
+    for child_metric in [eval_metric, cls_metric, bbox_metric]:
+        eval_metrics.add(child_metric)
+    optimizer_params = {'momentum': 0.9,
+                        'wd': 0.0005,
+                        'learning_rate': 0.001,
+                        'lr_scheduler': mx.lr_scheduler.FactorScheduler(30000, 0.1),
+                        'rescale_grad': (1.0 / config.TRAIN.BATCH_SIZE)}
+
     # train
-    solver = Solver(prefix, sym, ctx, begin_epoch, end_epoch, kv_store, args, auxs, momentum=0.9, wd=0.0005,
-                    learning_rate=1e-3, lr_scheduler=mx.lr_scheduler.FactorScheduler(30000, 0.1),
-                    mutable_data_shape=True, max_data_shape=max_data_shape)
-    solver.fit(train_data, frequent=frequent)
+    mod = MutableModule(sym, data_names=data_names, label_names=label_names,
+                        logger=logger, context=ctx, work_load_list=work_load_list,
+                        max_data_shapes=max_data_shape)
+    mod.fit(train_data, eval_metric=eval_metrics, epoch_end_callback=epoch_end_callback,
+            batch_end_callback=batch_end_callback, kvstore=kv_store,
+            optimizer='sgd', optimizer_params=optimizer_params,
+            arg_params=args, aux_params=auxs, begin_epoch=begin_epoch, num_epoch=end_epoch)
 
     # edit params and save
     for epoch in range(begin_epoch + 1, end_epoch + 1):
--- tools/train_rpn.py
+++ tools/train_rpn.py
@@ -4,15 +4,18 @@ import os
 
 import mxnet as mx
 
+from rcnn.callback import Speedometer
 from rcnn.config import config
 from rcnn.loader import AnchorLoader
-from rcnn.solver import Solver
+from rcnn.metric import AccuracyMetric, LogLossMetric, SmoothL1LossMetric
+from rcnn.module import MutableModule
 from rcnn.symbol import get_vgg_rpn
 from utils.load_data import load_gt_roidb
 from utils.load_model import load_param
 
 config.TRAIN.HAS_RPN = True
 config.TRAIN.BATCH_SIZE = 1
+config.TRAIN.ASPECT_GROUPING = False
 
 
 def train_net(image_set, year, root_path, devkit_path, pretrained, epoch,
@@ -35,7 +38,7 @@ def train_net(image_set, year, root_path, devkit_path, pretrained, epoch,
                               ctx=ctx, work_load_list=work_load_list)
 
     # infer max shape
-    max_data_shape = [('data', (1, 3, 1000, 1000))]
+    max_data_shape = [('data', (config.TRAIN.BATCH_SIZE, 3, 1000, 1000))]
     max_data_shape_dict = {k: v for k, v in max_data_shape}
     _, feat_shape, _ = feat_sym.infer_shape(**max_data_shape_dict)
     from rcnn.minibatch import assign_anchor
@@ -52,20 +55,51 @@ def train_net(image_set, year, root_path, devkit_path, pretrained, epoch,
 
     # initialize params
     if not resume:
-        arg_shape, _, _ = sym.infer_shape(data=(1, 3, 224, 224))
+        input_shapes = {k: v for k, v in train_data.provide_data + train_data.provide_label}
+        arg_shape, _, _ = sym.infer_shape(**input_shapes)
         arg_shape_dict = dict(zip(sym.list_arguments(), arg_shape))
-        args['rpn_conv_3x3_weight'] = mx.random.normal(mean=0, stdvar=0.01, shape=arg_shape_dict['rpn_conv_3x3_weight'])
+        args['rpn_conv_3x3_weight'] = mx.random.normal(0, 0.01, shape=arg_shape_dict['rpn_conv_3x3_weight'])
         args['rpn_conv_3x3_bias'] = mx.nd.zeros(shape=arg_shape_dict['rpn_conv_3x3_bias'])
-        args['rpn_cls_score_weight'] = mx.random.normal(mean=0, stdvar=0.01, shape=arg_shape_dict['rpn_cls_score_weight'])
+        args['rpn_cls_score_weight'] = mx.random.normal(0, 0.01, shape=arg_shape_dict['rpn_cls_score_weight'])
         args['rpn_cls_score_bias'] = mx.nd.zeros(shape=arg_shape_dict['rpn_cls_score_bias'])
-        args['rpn_bbox_pred_weight'] = mx.random.normal(mean=0, stdvar=0.01, shape=arg_shape_dict['rpn_bbox_pred_weight'])
+        args['rpn_bbox_pred_weight'] = mx.random.normal(0, 0.01, shape=arg_shape_dict['rpn_bbox_pred_weight'])
         args['rpn_bbox_pred_bias'] = mx.nd.zeros(shape=arg_shape_dict['rpn_bbox_pred_bias'])
 
+    # prepare training
+    fixed_params_names = []
+    for name in args.keys():
+        if config.TRAIN.FINETUNE and name.startswith('conv'):
+            fixed_params_names.append(name)
+        elif name.startswith('conv1') or name.startswith('conv2'):
+            fixed_params_names.append(name)
+    data_names = [k[0] for k in train_data.provide_data]
+    label_names = [k[0] for k in train_data.provide_label]
+    batch_end_callback = Speedometer(train_data.batch_size, frequent=frequent)
+    epoch_end_callback = mx.callback.do_checkpoint(prefix)
+    if config.TRAIN.HAS_RPN is True:
+        eval_metric = AccuracyMetric(use_ignore=True, ignore=-1)
+        cls_metric = LogLossMetric(use_ignore=True, ignore=-1)
+    else:
+        eval_metric = AccuracyMetric()
+        cls_metric = LogLossMetric()
+    bbox_metric = SmoothL1LossMetric()
+    eval_metrics = mx.metric.CompositeEvalMetric()
+    for child_metric in [eval_metric, cls_metric, bbox_metric]:
+        eval_metrics.add(child_metric)
+    optimizer_params = {'momentum': 0.9,
+                        'wd': 0.0005,
+                        'learning_rate': 0.001,
+                        'lr_scheduler': mx.lr_scheduler.FactorScheduler(60000, 0.1),
+                        'rescale_grad': (1.0 / config.TRAIN.BATCH_SIZE)}
+
     # train
-    solver = Solver(prefix, sym, ctx, begin_epoch, end_epoch, kv_store, args, auxs, momentum=0.9, wd=0.0005,
-                    learning_rate=1e-3, lr_scheduler=mx.lr_scheduler.FactorScheduler(60000, 0.1),
-                    mutable_data_shape=True, max_data_shape=max_data_shape, max_label_shape=max_label_shape)
-    solver.fit(train_data, frequent=frequent)
+    mod = MutableModule(sym, data_names=data_names, label_names=label_names,
+                        logger=logger, context=ctx, work_load_list=work_load_list,
+                        max_data_shapes=max_data_shape, max_label_shapes=max_label_shape)
+    mod.fit(train_data, eval_metric=eval_metrics, epoch_end_callback=epoch_end_callback,
+            batch_end_callback=batch_end_callback, kvstore=kv_store,
+            optimizer='sgd', optimizer_params=optimizer_params,
+            arg_params=args, aux_params=auxs, begin_epoch=begin_epoch, num_epoch=end_epoch)
 
 
 def parse_args():
