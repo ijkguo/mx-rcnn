562c581df3a74a96063c72a2d01d15bcbc00324c
Jian Guo <guojian710@gmail.com>
2018-05-17 17:53:52 -0700

    demo: mx image

--- demo.py
+++ demo.py
@@ -1,15 +1,13 @@
 import argparse
-import os
-import cv2
 import mxnet as mx
 import numpy as np
-from rcnn.logger import logger
+
 from rcnn.config import config
 from rcnn.symbol.symbol_resnet import get_resnet_test
-from rcnn.io.image import resize, transform
-from rcnn.core.tester import Predictor, im_detect, im_proposal, vis_all_detection
 from rcnn.utils.load_model import load_param
-from rcnn.processing.nms import py_nms_wrapper, cpu_nms_wrapper, gpu_nms_wrapper
+from rcnn.core.tester import Predictor
+from rcnn.processing.bbox_transform import bbox_pred, clip_boxes
+from rcnn.processing.nms import py_nms_wrapper
 
 
 CLASSES = ('__background__',
@@ -18,18 +16,58 @@ CLASSES = ('__background__',
            'cow', 'diningtable', 'dog', 'horse',
            'motorbike', 'person', 'pottedplant',
            'sheep', 'sofa', 'train', 'tvmonitor')
-config.TEST.HAS_RPN = True
+
 SHORT_SIDE = config.SCALES[0][0]
 LONG_SIDE = config.SCALES[0][1]
-PIXEL_MEANS = config.PIXEL_MEANS
+
+PIXEL_MEANS = (123.68, 116.779, 103.939)
+PIXEL_STDS = (1.0, 1.0, 1.0)
+
 DATA_NAMES = ['data', 'im_info']
 LABEL_NAMES = None
 DATA_SHAPES = [('data', (1, 3, LONG_SIDE, SHORT_SIDE)), ('im_info', (1, 3))]
 LABEL_SHAPES = None
-# visualization
+
 CONF_THRESH = 0.7
 NMS_THRESH = 0.3
-nms = py_nms_wrapper(NMS_THRESH)
+
+
+def imdecode(image_name):
+    """Return NDArray [height, width, channel]"""
+    import os
+    assert os.path.exists(image_name), image_name + ' not found'
+    with open(image_name, 'rb') as f:
+        buf = f.read()
+    im = mx.image.imdecode(buf)
+    return im
+
+
+def resize(im, short=SHORT_SIDE, max_size=LONG_SIDE):
+    """Return resized image (NDArray) and scale (float)"""
+    im_shape = im.shape
+    im_size_min = min(im_shape[0:2])
+    im_size_max = max(im_shape[0:2])
+    im_scale = float(short) / float(im_size_min)
+    # prevent bigger axis from being more than max_size:
+    if round(im_scale * im_size_max) > max_size:
+        im_scale = float(max_size) / float(im_size_max)
+    im = mx.image.resize_short(im, short)
+    return im, im_scale
+
+
+def transform(im, means=PIXEL_MEANS, stds=PIXEL_STDS):
+    """transform from RGB image into (C, H, W) tensor with RGB means and stds"""
+    im_tensor = im.transpose((2, 0, 1)).astype("float32")
+    im_tensor = mx.nd.image.normalize(im_tensor, means, stds)
+    return im_tensor
+
+
+def transform_inverse(im_tensor, means=PIXEL_MEANS, stds=PIXEL_STDS):
+    """transform from (C, H, W) tensor to RGB image"""
+    means = mx.nd.array(means).reshape((1, 3))
+    stds = mx.nd.array(stds).reshape((1, 3))
+    im = im_tensor.transpose((1, 2, 0)) * stds + means
+    return im
 
 
 def get_net(symbol, prefix, epoch, ctx):
@@ -60,81 +98,125 @@ def get_net(symbol, prefix, epoch, ctx):
     return predictor
 
 
-def generate_batch(im):
-    """
-    preprocess image, return batch
-    :param im: cv2.imread returns [height, width, channel] in BGR
-    :return:
-    data_batch: MXNet input batch
-    data_names: names in data_batch
-    im_scale: float number
-    """
-    im_array, im_scale = resize(im, SHORT_SIDE, LONG_SIDE)
-    im_array = transform(im_array, PIXEL_MEANS)
-    im_info = np.array([[im_array.shape[2], im_array.shape[3], im_scale]], dtype=np.float32)
-    data = [mx.nd.array(im_array), mx.nd.array(im_info)]
-    data_shapes = [('data', im_array.shape), ('im_info', im_info.shape)]
+def generate_batch(im_tensor, im_info):
+    """return batch"""
+    # for 1-batch inference purpose, cannot use batchify (or nd.stack) to expand dims
+    im_tensor = im_tensor.expand_dims(0)
+    im_info = im_info.expand_dims(0)
+    data = [im_tensor, im_info]
+    data_shapes = [('data', im_tensor.shape), ('im_info', im_info.shape)]
     data_batch = mx.io.DataBatch(data=data, label=None, provide_data=data_shapes, provide_label=None)
-    return data_batch, DATA_NAMES, im_scale
+    return data_batch
 
 
-def demo_net(predictor, image_name, vis=False):
+def im_detect(predictor, data_batch):
+    """use predictor"""
+    # read input
+    data_dict = {dshape[0]: datum for dshape, datum in zip(data_batch.provide_data, data_batch.data)}
+    height, width, scale = data_dict['im_info'].asnumpy()[0]
+
+    # save output
+    output = predictor.predict(data_batch)
+    rois = output['rois_output'].asnumpy()[:, 1:]
+    scores = output['cls_prob_reshape_output'].asnumpy()[0]
+    bbox_deltas = output['bbox_pred_reshape_output'].asnumpy()[0]
+
+    # post processing
+    pred_boxes = bbox_pred(rois, bbox_deltas)
+    pred_boxes = clip_boxes(pred_boxes, (height, width))
+
+    # we used scaled image to infer, so it is necessary to transform them back
+    pred_boxes = pred_boxes / scale
+
+    return scores, pred_boxes
+
+
+def vis_all_detection(im_array, detections, class_names):
     """
-    generate data_batch -> im_detect -> post process
-    :param predictor: Predictor
-    :param image_name: image name
-    :param vis: will save as a new image if not visualized
-    :return: None
+    visualize all detections in one image
+    :param im_array: [b=1 c h w] in rgb
+    :param detections: [ numpy.ndarray([[x1 y1 x2 y2 score]]) for j in classes ]
+    :param class_names: list of names in imdb
+    :return:
     """
-    assert os.path.exists(image_name), image_name + ' not found'
-    im = cv2.imread(image_name)
-    data_batch, data_names, im_scale = generate_batch(im)
-    scores, boxes, data_dict = im_detect(predictor, data_batch, data_names, im_scale)
-
-    all_boxes = [[] for _ in CLASSES]
-    for cls in CLASSES:
-        cls_ind = CLASSES.index(cls)
-        cls_boxes = boxes[:, 4 * cls_ind:4 * (cls_ind + 1)]
-        cls_scores = scores[:, cls_ind, np.newaxis]
-        keep = np.where(cls_scores >= CONF_THRESH)[0]
-        dets = np.hstack((cls_boxes, cls_scores)).astype(np.float32)[keep, :]
-        keep = nms(dets)
-        all_boxes[cls_ind] = dets[keep, :]
-
-    boxes_this_image = [[]] + [all_boxes[j] for j in range(1, len(CLASSES))]
-
-    # print results
-    logger.info('---class---')
-    logger.info('[[x1, x2, y1, y2, confidence]]')
-    for ind, boxes in enumerate(boxes_this_image):
-        if len(boxes) > 0:
-            logger.info('---%s---' % CLASSES[ind])
-            logger.info('%s' % boxes)
-
-    image = data_dict['data'].asnumpy()
-    result_file = image_name.replace('.', '_result.')
-    logger.info('results saved to %s' % result_file)
-    vis_all_detection(image, boxes_this_image, CLASSES, im_scale, save=not vis, filename=result_file)
+    import matplotlib.pyplot as plt
+    import random
+    plt.imshow(im_array)
+    for j, name in enumerate(class_names):
+        if name == '__background__':
+            continue
+        color = (random.random(), random.random(), random.random())  # generate a random color
+        dets = detections[j]
+        for det in dets:
+            bbox = det[:4]
+            score = det[-1]
+            rect = plt.Rectangle((bbox[0], bbox[1]),
+                                 bbox[2] - bbox[0],
+                                 bbox[3] - bbox[1], fill=False,
+                                 edgecolor=color, linewidth=3.5)
+            plt.gca().add_patch(rect)
+            plt.gca().text(bbox[0], bbox[1] - 2,
+                           '{:s} {:.3f}'.format(name, score),
+                           bbox=dict(facecolor=color, alpha=0.5), fontsize=12, color='white')
+    plt.show()
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Demonstrate a Faster R-CNN network')
-    parser.add_argument('prefix', help='saved model prefix', type=str)
-    parser.add_argument('epoch', help='epoch of pretrained model', type=int)
+    parser.add_argument('prefix', help='saved model prefix', default="model/resnet_voc0712", type=str)
+    parser.add_argument('epoch', help='epoch of pretrained model', default=10, type=int)
     parser.add_argument('gpu', help='GPU device to use', default=0, type=int)
-    parser.add_argument('image', help='custom image', type=str)
+    parser.add_argument('image', help='custom image', default="street_small.jpg", type=str)
     parser.add_argument('--vis', help='display result', action='store_true')
     args = parser.parse_args()
     return args
 
 
-def main():
-    args = parse_args()
-    ctx = mx.gpu(args.gpu)
-    symbol = get_resnet_test(num_classes=config.NUM_CLASSES, num_anchors=config.NUM_ANCHORS)
-    predictor = get_net(symbol, args.prefix, args.epoch, ctx)
-    demo_net(predictor, args.image, args.vis)
+# parse args
+args = parse_args()
+ctx = mx.gpu(args.gpu)
 
+# decode and resize image
+im_orig = imdecode(args.image)
+im, im_scale = resize(im_orig)
 
-if __name__ == '__main__':
-    main()
+height, width = im.shape[:2]
+im_info = mx.nd.array([height, width, im_scale])
+
+# transform into tensor
+im_tensor = transform(im)
+
+# generate data batch
+data_batch = generate_batch(im_tensor, im_info)
+
+# assemble executor
+symbol = get_resnet_test(num_classes=config.NUM_CLASSES, num_anchors=config.NUM_ANCHORS)
+predictor = get_net(symbol, args.prefix, args.epoch, ctx)
+
+# forward
+scores, pred_boxes = im_detect(predictor, data_batch)
+
+# convert to per class detection results
+nms = py_nms_wrapper(NMS_THRESH)
+all_boxes = [[] for _ in CLASSES]
+for cls in CLASSES:
+    cls_ind = CLASSES.index(cls)
+    cls_boxes = pred_boxes[:, 4 * cls_ind:4 * (cls_ind + 1)]
+    cls_scores = scores[:, cls_ind, np.newaxis]
+    keep = np.where(cls_scores >= CONF_THRESH)[0]
+    dets = np.hstack((cls_boxes, cls_scores)).astype(np.float32)[keep, :]
+    keep = nms(dets)
+    all_boxes[cls_ind] = dets[keep, :]
+boxes_this_image = [[]] + [all_boxes[j] for j in range(1, len(CLASSES))]
+
+# print results
+print('---class---')
+print('[[x1, x2, y1, y2, confidence]]')
+for ind, boxes in enumerate(boxes_this_image):
+    if len(boxes) > 0:
+        print('---%s---' % CLASSES[ind])
+        print('%s' % boxes)
+
+# if vis
+if args.vis:
+    vis_all_detection(im_orig.asnumpy(), boxes_this_image, CLASSES)
