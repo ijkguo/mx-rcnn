61f7b23662bf444902e3669431780e852eb4ce55
Jian Guo <guojian710@gmail.com>
2018-05-18 18:03:32 -0700

    reorg: remove extra stuff

--- script/additional_deps.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# install additional depts
-sudo apt install python-pip python-dev unzip python-matplotlib
-sudo pip install cython scikit-image easydict opencv-python
-
-# build cython extension
-make
