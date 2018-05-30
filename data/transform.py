e004f7dcffdb5895b3868e9fba31c51134abc266
Jian Guo <guojian710@gmail.com>
2018-05-29 21:01:32 -0700

    training: fix data transform

--- data/transform.py
+++ data/transform.py
@@ -66,23 +66,35 @@ class RCNNDefaultTrainTransform(object):
         # transform into tensor and normalize
         im_tensor = transform(im, self._mean, self._std)
 
-        # label is gt_boxes [n, 6] (x1, y1, x2, y2, cls, difficult)
+        # label is (np.array) gt_boxes [n, 6] (x1, y1, x2, y2, cls, difficult)
         # proposal target input (x1, y1, x2, y2)
-        gt_bboxes = label.slice_axis(axis=-1, begin=0, end=5)
+        # important: need to copy this np.array (numpy will keep this array)
+        gt_bboxes = label[:, :5].copy()
 
         # resize bbox
         gt_bboxes[:, :4] *= im_scale
 
         # random flip image and bbox
         im, flip_x = random_flip(im, px=0.5)
+        # invalid value in bbox_transform if this wrong (no overlap), note index 0 and 2
         if flip_x:
-            gt_bboxes[:, 0] = im_height + 1 - gt_bboxes[:, 0]
-            gt_bboxes[:, 2] = im_height + 1 - gt_bboxes[:, 2]
+            gt_bboxes[:, 0] = im_width - 1 - gt_bboxes[:, 2]
+            gt_bboxes[:, 2] = im_width - 1 - gt_bboxes[:, 0]
 
         # compute anchor shape and generate anchors
-        feat_height, feat_width = self._ac(im_height, im_width)
+        feat_height, feat_width = self._ac(im_height), self._ac(im_width)
         anchors = self._ag.generate(feat_height, feat_width)
 
         # assign anchors
-        rpn_label, bbox_target, bbox_weight = self._asp.assign(anchors, gt_bboxes.asnumpy(), im_height, im_width)
+        rpn_label, bbox_target, bbox_weight = self._asp.assign(anchors, gt_bboxes, im_height, im_width)
+
+        rpn_label = rpn_label.reshape((feat_height, feat_width, -1)).transpose((2, 0, 1)).flatten()
+        bbox_target = bbox_target.reshape((feat_height, feat_width, -1)).transpose((2, 0, 1))
+        bbox_weight = bbox_weight.reshape((feat_height, feat_width, -1)).transpose((2, 0, 1))
+
+        # convert to ndarray
+        gt_bboxes = mx.nd.array(gt_bboxes)
+        rpn_label = mx.nd.array(rpn_label)
+        bbox_target = mx.nd.array(bbox_target)
+        bbox_weight = mx.nd.array(bbox_weight)
         return im_tensor, im_info, gt_bboxes, rpn_label, bbox_target, bbox_weight
