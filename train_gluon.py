b504052244d6b4d9dee5da536df35d8c9ea08a4c
Jian Guo <guojian710@gmail.com>
2018-06-04 15:33:10 -0700

    train fix loss explode

--- train_gluon.py
+++ train_gluon.py
@@ -150,8 +150,8 @@ def main():
                     rpn_loss1 = rpn_cls_loss(rpn_cls, rpn_label, rpn_weight) * rpn_weight.size
                     rpn_loss2 = rpn_reg_loss(rpn_reg, rpn_bbox_target, rpn_bbox_weight) * rpn_bbox_weight.size
                     # rcnn loss
-                    rcnn_loss1 = rcnn_cls_loss(rcnn_cls, rcnn_label) * rcnn_label.size
-                    rcnn_loss2 = rcnn_reg_loss(rcnn_reg, rcnn_bbox_target, rcnn_bbox_weight) * rcnn_bbox_weight.size
+                    rcnn_loss1 = rcnn_cls_loss(rcnn_cls, rcnn_label)
+                    rcnn_loss2 = rcnn_reg_loss(rcnn_reg, rcnn_bbox_target, rcnn_bbox_weight) * rcnn_bbox_weight.shape[-1]
                     # loss for backprop
                     losses.append(rpn_loss1.sum() + rpn_loss2.sum() + rcnn_loss1.sum() + rcnn_loss2.sum())
                     # loss for metrics
