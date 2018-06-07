a968c69cb1805d767225c825974f9fda4f907f4c
Jian Guo <ijkguo@gmail.com>
2018-06-07 14:52:36 -0700

    move pascal voc eval

--- dataset/pascal_voc.py
+++ dataset/pascal_voc.py
@@ -4,7 +4,6 @@ import numpy as np
 
 from net.logger import logger
 from .imdb import IMDB
-from .pascal_voc_eval import voc_eval
 
 
 class PascalVOC(IMDB):
@@ -158,10 +157,95 @@ class PascalVOC(IMDB):
             bbox = np.array(bbox)
             confidence = np.array(confidence)
 
-            rec, prec, ap = voc_eval(class_anno, npos, image_ids, bbox, confidence,
-                                     ovthresh=0.5, use_07_metric=use_07_metric)
+            rec, prec, ap = self.voc_eval(class_anno, npos, image_ids, bbox, confidence,
+                                          ovthresh=0.5, use_07_metric=use_07_metric)
             aps.append(ap)
 
         for cls, ap in zip(self.classes, aps):
             logger.info('AP for {} = {:.4f}'.format(cls, ap))
         logger.info('Mean AP = {:.4f}'.format(np.mean(aps)))
+
+    @staticmethod
+    def voc_eval(class_anno, npos, image_ids, bbox, confidence, ovthresh=0.5, use_07_metric=False):
+        # sort by confidence
+        if bbox.shape[0] > 0:
+            sorted_inds = np.argsort(-confidence)
+            sorted_scores = np.sort(-confidence)
+            bbox = bbox[sorted_inds, :]
+            image_ids = [image_ids[x] for x in sorted_inds]
+
+        # go down detections and mark true positives and false positives
+        nd = len(image_ids)
+        tp = np.zeros(nd)
+        fp = np.zeros(nd)
+        for d in range(nd):
+            r = class_anno[image_ids[d]]
+            bb = bbox[d, :].astype(float)
+            ovmax = -np.inf
+            bbgt = r['bbox'].astype(float)
+
+            if bbgt.size > 0:
+                # compute overlaps
+                # intersection
+                ixmin = np.maximum(bbgt[:, 0], bb[0])
+                iymin = np.maximum(bbgt[:, 1], bb[1])
+                ixmax = np.minimum(bbgt[:, 2], bb[2])
+                iymax = np.minimum(bbgt[:, 3], bb[3])
+                iw = np.maximum(ixmax - ixmin + 1., 0.)
+                ih = np.maximum(iymax - iymin + 1., 0.)
+                inters = iw * ih
+
+                # union
+                uni = ((bb[2] - bb[0] + 1.) * (bb[3] - bb[1] + 1.) +
+                       (bbgt[:, 2] - bbgt[:, 0] + 1.) *
+                       (bbgt[:, 3] - bbgt[:, 1] + 1.) - inters)
+
+                overlaps = inters / uni
+                ovmax = np.max(overlaps)
+                jmax = np.argmax(overlaps)
+
+            if ovmax > ovthresh:
+                if not r['difficult'][jmax]:
+                    if not r['det'][jmax]:
+                        tp[d] = 1.
+                        r['det'][jmax] = 1
+                    else:
+                        fp[d] = 1.
+            else:
+                fp[d] = 1.
+
+        # compute precision recall
+        fp = np.cumsum(fp)
+        tp = np.cumsum(tp)
+        rec = tp / float(npos)
+        # avoid division by zero in case first detection matches a difficult ground ruth
+        prec = tp / np.maximum(tp + fp, np.finfo(np.float64).eps)
+        ap = PascalVOC.voc_ap(rec, prec, use_07_metric)
+
+        return rec, prec, ap
+
+    @staticmethod
+    def voc_ap(rec, prec, use_07_metric=False):
+        if use_07_metric:
+            ap = 0.
+            for t in np.arange(0., 1.1, 0.1):
+                if np.sum(rec >= t) == 0:
+                    p = 0
+                else:
+                    p = np.max(prec[rec >= t])
+                ap += p / 11.
+        else:
+            # append sentinel values at both ends
+            mrec = np.concatenate(([0.], rec, [1.]))
+            mpre = np.concatenate(([0.], prec, [0.]))
+
+            # compute precision integration ladder
+            for i in range(mpre.size - 1, 0, -1):
+                mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+            # look for recall value changes
+            i = np.where(mrec[1:] != mrec[:-1])[0]
+
+            # sum (\delta recall) * prec
+            ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+        return ap
