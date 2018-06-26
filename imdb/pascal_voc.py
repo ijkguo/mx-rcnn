81fd0720dc7d3db177c28918962e6e9c7ed17343
Jian Guo <ijkguo@gmail.com>
2018-06-25 18:53:33 -0700

    fix voc eval bug

--- imdb/pascal_voc.py
+++ imdb/pascal_voc.py
@@ -77,7 +77,7 @@ class PascalVOC(IMDB):
             gt_classes[ix] = cls
 
         roi_rec = {'index': index,
-                   'objs': objs,
+                   'objs': orig_objs,
                    'image': self._image_file_tmpl.format(index),
                    'height': height,
                    'width': width,
