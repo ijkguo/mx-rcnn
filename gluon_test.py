982a7b5fc1caa530cb4afeaa73a32575951aeaf4
Jian Guo <ijkguo@gmail.com>
2018-06-27 13:46:21 -0700

    multi testing

--- gluon_test.py
+++ gluon_test.py
@@ -9,7 +9,7 @@ from gluoncv.utils.metrics.voc_detection import VOC07MApMetric
 from tqdm import tqdm
 
 from nddata.bbox import decode_detect
-from nddata.transform import RCNNDefaultValTransform
+from nddata.transform import RCNNDefaultValTransform, batchify_append, batchify_pad, split_append, split_pad
 from symdata.anchor import AnchorGenerator
 from symnet.logger import logger
 
@@ -18,8 +18,13 @@ def test_net(net, dataset, args):
     # print config
     logger.info('called with args\n{}'.format(pprint.pformat(vars(args))))
 
-    # setup context
-    ctx = mx.gpu(args.gpu)
+    # setup multi-gpu
+    ctx = [mx.gpu(int(i)) for i in args.gpus.split(',')]
+    batch_size = args.rcnn_batch_size * len(ctx)
+    if args.rcnn_batch_size == 1:
+        batchify_fn, split_fn = batchify_append, split_append
+    else:
+        batchify_fn, split_fn = batchify_pad, split_pad
 
     # load testing data
     ag = AnchorGenerator(feat_stride=args.rpn_feat_stride,
@@ -28,7 +33,8 @@ def test_net(net, dataset, args):
                                             mean=args.img_pixel_means, std=args.img_pixel_stds,
                                             feat_stride=args.rpn_feat_stride, ag=ag)
     val_loader = gluon.data.DataLoader(dataset.transform(val_transform),
-                                       batch_size=1, shuffle=False, last_batch="keep", num_workers=4)
+                                       batch_size=batch_size, shuffle=False, batchify_fn=batchify_fn,
+                                       last_batch="keep", num_workers=4)
 
     # load model
     net.load_parameters(args.params)
@@ -39,32 +45,45 @@ def test_net(net, dataset, args):
     metric = VOC07MApMetric(iou_thresh=0.5, class_names=dataset.classes)
     with tqdm(total=len(dataset)) as pbar:
         for ib, batch in enumerate(val_loader):
-            im_tensor = batch[0].as_in_context(ctx)
-            anchors = batch[1].as_in_context(ctx)
-            im_info = batch[2].as_in_context(ctx)
-            label = batch[3].as_in_context(ctx)
+            batch = split_fn(batch, ctx)
 
-            gt_ids = label.slice_axis(axis=-1, begin=4, end=5)
-            gt_bboxes = label.slice_axis(axis=-1, begin=0, end=4)
-            gt_difficults = label.slice_axis(axis=-1, begin=5, end=6) if label.shape[-1] > 5 else None
+            # lazy eval
+            det_bboxes = []
+            det_ids = []
+            det_scores = []
+            gt_bboxes = []
+            gt_ids = []
+            gt_difficults = []
 
-            # forward
-            rois, scores, bbox_deltas = net(im_tensor, anchors, im_info)
-            rois = rois[:, 1:]
-            scores = mx.nd.softmax(scores)
-            im_info = im_info[0]
+            for data, anchors, im_info, label in zip(*batch):
+                # append all labels
+                gt_bboxes.append(label.slice_axis(axis=-1, begin=0, end=4))
+                gt_ids.append(label.slice_axis(axis=-1, begin=4, end=5))
+                gt_difficults.append(label.slice_axis(axis=-1, begin=5, end=6) if label.shape[-1] > 5 else None)
 
-            # post processing
-            det = decode_detect(rois, scores, bbox_deltas, im_info,
-                                bbox_stds=args.rcnn_bbox_stds, nms_thresh=args.rcnn_nms_thresh)
-            cls = det.slice_axis(axis=-1, begin=0, end=1)
-            conf = det.slice_axis(axis=-1, begin=1, end=2)
-            boxes = det.slice_axis(axis=-1, begin=2, end=6)
-            cls -= 1
+                # forward
+                rois, scores, bbox_deltas = net(data, anchors, im_info)
+                rois = rois[:, 1:]
+                scores = mx.nd.softmax(scores)
+                im_info = im_info[0]
 
-            metric.update(boxes.expand_dims(0), cls.expand_dims(0), conf.expand_dims(0),
-                          gt_bboxes, gt_ids, gt_difficults)
-            pbar.update(batch[0].shape[0])
+                # post processing
+                det = decode_detect(rois, scores, bbox_deltas, im_info,
+                                    bbox_stds=args.rcnn_bbox_stds, nms_thresh=args.rcnn_nms_thresh)
+                cls = det.slice_axis(axis=-1, begin=0, end=1)
+                conf = det.slice_axis(axis=-1, begin=1, end=2)
+                boxes = det.slice_axis(axis=-1, begin=2, end=6)
+                cls -= 1
+
+                # append all results
+                det_bboxes.append(boxes.expand_dims(0))
+                det_ids.append(cls.expand_dims(0))
+                det_scores.append(conf.expand_dims(0))
+
+            for det_bbox, det_id, det_score, gt_bbox, gt_id, gt_diffcult in zip(
+                    det_bboxes, det_ids, det_scores, gt_bboxes, gt_ids, gt_difficults):
+                metric.update(det_bbox, det_id, det_score, gt_bbox, gt_id, gt_diffcult)
+            pbar.update(batch_size)
     names, values = metric.get()
 
     # print
@@ -79,7 +98,7 @@ def parse_args():
     parser.add_argument('--params', type=str, default='', help='path to trained model')
     parser.add_argument('--dataset', type=str, default='voc', help='training dataset')
     parser.add_argument('--imageset', type=str, default='', help='imageset splits')
-    parser.add_argument('--gpu', type=int, default=0, help='gpu device eg. 0')
+    parser.add_argument('--gpus', type=str, default='0', help='gpu devices eg. 0,1')
     # faster rcnn params
     parser.add_argument('--img-short-side', type=int, default=600)
     parser.add_argument('--img-long-side', type=int, default=1000)
