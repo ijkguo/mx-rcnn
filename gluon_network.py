085f17ccb3d0777b34b3cd781e62891239100f4f
Jian Guo <ijkguo@gmail.com>
2018-07-20 14:53:51 -0700

    add resnetv1

--- gluon_network.py
+++ gluon_network.py
@@ -1,3 +1,30 @@
+class ResNetV1a:
+    def __init__(self, is_train):
+        from ndnet.net_rcnn import get_frcnn_resnet50_v1a
+        self._is_train = is_train
+        self._net_cls = get_frcnn_resnet50_v1a
+
+    def set_args(self, args):
+        if self._is_train:
+            args.net_train_patterns = '|'.join(['.*rpn', '.*dense', '.*stage(2|3|4)_conv'])
+            args.pretrained = args.pretrained if args.pretrained else 'model/resnet50_v1a_0000.params'
+        args.img_pixel_means = (122.7717, 115.9465, 102.9801)
+        args.img_pixel_stds = (1.0, 1.0, 1.0)
+        args.rpn_feat_stride = 16
+        args.rcnn_feat_stride = 16
+        args.rcnn_pooled_size = (14, 14)
+
+    def get_net(self, args):
+        net = self._net_cls(
+            rpn_feature_stride=args.rpn_feat_stride, rpn_anchor_scales=args.rpn_anchor_scales, rpn_anchor_ratios=args.rpn_anchor_ratios,
+            rpn_pre_topk=args.rpn_pre_nms_topk, rpn_post_topk=args.rpn_post_nms_topk, rpn_nms_thresh=args.rpn_nms_thresh, rpn_min_size=args.rpn_min_size,
+            rcnn_feature_stride=args.rcnn_feat_stride, rcnn_pooled_size=args.rcnn_pooled_size, rcnn_roi_mode='align',
+            rcnn_num_classes=args.rcnn_num_classes, rcnn_batch_size=args.rcnn_batch_size, rcnn_batch_rois=args.rcnn_batch_rois, rcnn_bbox_stds=args.rcnn_bbox_stds,
+            rcnn_fg_fraction=args.rcnn_fg_fraction, rcnn_fg_overlap=args.rcnn_fg_overlap,
+            rcnn_nms_thresh=args.rcnn_nms_thresh, rcnn_nms_topk=args.rcnn_nms_topk)
+        return net
+
+
 class ResNetV2a:
     def __init__(self, is_train):
         from ndnet.net_rcnn import get_frcnn_resnet50_v2a
@@ -27,6 +54,7 @@ class ResNetV2a:
 
 class NetworkFactory:
     NETWORKS = {
+        'resnet50_v1a': ResNetV1a,
         'resnet50_v2a': ResNetV2a
     }
     def __init__(self, network):
---
+++ ndnet/net_resnet_v1a.py
@@ -0,0 +1,70 @@
+from mxnet.gluon import nn, HybridBlock
+
+class BottleneckV1(HybridBlock):
+    def __init__(self, channels, stride, downsample=False, in_channels=0, **kwargs):
+        super(BottleneckV1, self).__init__(**kwargs)
+        self.conv1 = nn.Conv2D(channels // 4, kernel_size=1, strides=stride, use_bias=False)
+        self.bn1 = nn.BatchNorm(use_global_stats=True)
+        self.conv2 = nn.Conv2D(channels // 4, kernel_size=3, strides=1, padding=1, use_bias=False)
+        self.bn2 = nn.BatchNorm(use_global_stats=True)
+        self.conv3 = nn.Conv2D(channels, kernel_size=1, strides=1, use_bias=False)
+        self.bn3 = nn.BatchNorm(use_global_stats=True)
+        if downsample:
+            self.downsample = nn.HybridSequential(prefix='down_')
+            self.downsample.add(nn.Conv2D(channels, kernel_size=1, strides=stride, use_bias=False, in_channels=in_channels))
+            self.downsample.add(nn.BatchNorm(use_global_stats=True))
+        else:
+            self.downsample = None
+        self.relu = nn.Activation('relu')
+
+    def hybrid_forward(self, F, x):
+        residual = x
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        if self.downsample:
+            residual = self.downsample(residual)
+        x = F.Activation(x + residual, act_type='relu')
+        return x
+
+
+class ResNetV1a(HybridBlock):
+    def __init__(self, layers, **kwargs):
+        super(ResNetV1a, self).__init__(**kwargs)
+        with self.name_scope():
+            self.conv1 = nn.Conv2D(64, kernel_size=7, strides=2, padding=3, use_bias=False)
+            self.bn1 = nn.BatchNorm(use_global_stats=True)
+            self.relu = nn.Activation('relu')
+            self.maxpool = nn.MaxPool2D(pool_size=3, strides=2, padding=1)
+
+            self.layer1 = self._make_layer(stage_index=1, layers=layers[0], in_channels=64, channels=256, stride=1)
+            self.layer2 = self._make_layer(stage_index=2, layers=layers[1], in_channels=256, channels=512, stride=2)
+            self.layer3 = self._make_layer(stage_index=3, layers=layers[2], in_channels=512, channels=1024, stride=2)
+            self.layer4 = self._make_layer(stage_index=4, layers=layers[3], in_channels=1024, channels=2048, stride=2)
+
+            self.layer4.add(nn.GlobalAvgPool2D())
+            self.layer4.add(nn.Flatten())
+
+    def _make_layer(self, stage_index, layers, channels, stride, in_channels=0):
+        layer = nn.HybridSequential(prefix='stage%d_' % stage_index)
+        with layer.name_scope():
+            layer.add(BottleneckV1(channels, stride, channels != in_channels, in_channels=in_channels, prefix=''))
+            for _ in range(layers - 1):
+                layer.add(BottleneckV1(channels, 1, False, in_channels=channels, prefix=''))
+        return layer
+
+    def hybrid_forward(self, F, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        return x
