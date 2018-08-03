32c440b9ed0dd002017cb45de4db01b12eaf86fe
Jian Guo <ijkguo@gmail.com>
2018-08-03 16:52:50 -0700

    fix coco seg metric iter through bboxes

--- ndimdb/coco_metric.py
+++ ndimdb/coco_metric.py
@@ -24,25 +24,33 @@ class COCOSegmentationMetric(mx.metric.EvalMetric):
             else:
                 return a
 
-        bbox, label, score, mask = [_asnumpy(x) for x in [bboxes, labels, scores, masks]]
-        valid_pred = np.where((label >= 0) & (score >= 0.001))[0]
-        label = label.flat[valid_pred].astype('int32')
-        score = score.flat[valid_pred].astype('float32')
+        # mask must be the same as image shape, so no batch dimension is supported
+        bboxes, labels, scores, masks = [_asnumpy(x) for x in [bboxes, labels, scores, masks]]
+        # filter out padded detection & low confidence detections
+        valid_pred = np.where((labels >= 0) & (scores >= 0.001))[0]
+        labels = labels.flat[valid_pred].astype('int32')
+        scores = scores.flat[valid_pred].astype('float32')
+        bboxes = bboxes[valid_pred].astype('float32')
+        masks = masks[valid_pred].astype('uint8')
 
-        bbox = bbox[valid_pred].astype('float32')
-        bbox[2:4] -= bbox[:2]
-
-        mask = mask[valid_pred].astype('uint8')
-        rle = cocomask.encode(np.array(mask[:, :, np.newaxis], order='F'))[0]
-        rle['counts'] = rle['counts'].decode('ascii')
-
-        res = {'image_id': self._dataset.roidb[self._current_id]['image_id'],
-               'category_id': self._dataset.class_ind_to_coco_ind[label],
-               'bbox': list(map(lambda x: float(round(x, 2)), bbox)),
-               'score': float(round(score, 3)),
-               'segmentation': rle}
-        self._all_results.append(res)
+        # read image information from dataset
+        image_id = self._dataset.roidb[self._current_id]['image_id']
         self._current_id += 1
+
+        # for each bbox detection in this image
+        for bbox, label, score, mask in zip(bboxes, labels, scores, masks):
+            # coco evaluate on fp box (xmin, ymin, width, height)
+            bbox[2:4] -= bbox[:2]
+            # coco format full image mask to rle
+            rle = cocomask.encode(np.array(mask[:, :, np.newaxis], order='F'))[0]
+            rle['counts'] = rle['counts'].decode('ascii')
+
+            res = {'image_id': image_id,
+                   'category_id': self._dataset.class_ind_to_coco_ind[label],
+                   'bbox': list(map(lambda x: float(round(x, 2)), bbox)),
+                   'score': float(round(score, 3)),
+                   'segmentation': rle}
+            self._all_results.append(res)
 
     def get(self):
         with open(self._result_filename, 'w') as f:
